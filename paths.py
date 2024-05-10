c41e45cf1ae03291a11c9203750a488e4c1b73ab
Mathew Madhavacheril <mathewsyriac@gmail.com>
2024-05-10 13:28:21 -0400

    fix merge conflicts

--- paths.py
+++
@@ -1,4 +0,0 @@
-# Change these paths
-act_root = "/home/r/rbond/alague/scratch/ksz-pipeline/act_data_and_sims"
-out_dir =  "/home/r/rbond/alague/scratch/ksz-pipeline/ksz-analysis/quadratic_estimator/development_code/prepared_maps/"
-planck_root = "/home/r/rbond/alague/scratch/ksz-pipeline/Planck_data/"
